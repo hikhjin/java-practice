2f186ec9ac025571153c9be69da139cb8a814901
hikhjin <hikhjin@gmail.com>
2023-12-01 00:24:01 +0900

    loop

---
+++ src/section5/LoopEx1.java
@@ -0,0 +1,15 @@
+package section5;
+
+public class LoopEx1 {
+    public static void main(String[] args) {
+        /* int num = 2;
+        while(num < 20) {
+            num += 2;
+            System.out.println(num);
+        }
+         */
+        for (int num = 2, count = 0; count < 10; count++, num += 2) {
+            System.out.println(num);
+        }
+    }
+}
